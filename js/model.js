d386d6e4911f65c1847823bef1736b090f68cbe6
JoseA-Carrera <jose2252017@gmail.com>
2022-02-23 21:30:09 -0500

    Local Storage

--- js/model.js
+++ js/model.js
@@ -1,12 +1,28 @@
 export default class Model {
   constructor() {
     this.view = null;
-    this.todos = [];
-    this.currentId = 1;
+    this.todos = JSON.parse(localStorage.getItem('todos'));
+    if (!this.todos || this.todos.length < 1) {
+      this.todos = [{
+        id: 0,
+        title: 'noObject',
+        description: 'nobody',
+        completed: false
+      }]
+      this.currentId = 1;
+    } else {
+      this.currentId = this.todos[this.todos.length - 1].id + 1
+    }
+
   }
   setView(view) {
     this.view = view;
   }
+
+  save() {
+    localStorage.setItem('todos', JSON.stringify(this.todos));
+  }
+
   getTodos() {
     return this.todos;
   }
@@ -19,8 +35,13 @@ export default class Model {
     const index = this.findTodo(id);
     const todo = this.todos[index];
     todo.completed = !todo.completed;
-    console.log(this.todos);
+    this.save();
 
+  }
+  editTodo(id, values) {
+    const index = this.findTodo(id)
+    Object.assign(this.todos[index], values);
+    this.save()
   }
 
   addTodo(title, description) {
@@ -32,6 +53,7 @@ export default class Model {
     }
     this.todos.push(todo);
     console.log(this.todos);
+    this.save();
 
     return {
       ...todo
@@ -41,5 +63,6 @@ export default class Model {
   removeTodo(id) {
     const index = this.findTodo(id);
     this.todos.splice(index, 1);
+    this.save();
   }
 }
